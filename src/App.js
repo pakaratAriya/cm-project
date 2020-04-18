9f5f0d3851d1c3226469d533ec22eded8d9fa42c
pakaratAriya <pakarat_ariya@hotmail.com>
2020-04-17 19:56:04 -0700

    init the project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import classes from './App.module.css';
+
+function App() {
+  return (
+    <div>
+      Landing page goes here...
+    </div>
+  );
+}
+
+export default App;
