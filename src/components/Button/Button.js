bed2585c0317fb51c255cbaa3ac0606f7b4eec08
Kim Cyrus Esquibel <kimcyruse@Kims-Air.hitronhub.home>
2020-05-14 02:06:41 -0700

    Added largeBtn

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,30 +1,27 @@
-import React from 'react';
-import fonts from '../../globalCss/Fonts.module.css'
-import classes from './Button.module.css'
+const button = (props) => {
+  let myClass = [];
+  myClass.push(classes.Button);
+  myClass.push(fonts.Caption);
+  if (props.myClass.includes("goldBorder")) myClass.push(classes.GoldBorder);
+  if (props.myClass.includes("goldBackground"))
+    myClass.push(classes.GoldBackground);
+  if (props.myClass.includes("transparentBackground"))
+    myClass.push(classes.TransparentBackground);
+  if (props.myClass.includes("whiteText")) myClass.push(fonts.White);
+  if (props.myClass.includes("blackText")) myClass.push(fonts.Black);
+  if (props.myClass.includes("goldText")) myClass.push(fonts.Gold);
+  if (props.myClass.includes("smallBtn")) myClass.push(classes.SmallButton);
+  if (props.myClass.includes("mediumBtn")) myClass.push(classes.MediumButton);
+  if (props.myClass.includes("largeBtn")) myClass.push(classes.LargeButton);
+  return (
+    <div
+      className={myClass.join(" ")}
+      style={props.style}
+      onClick={props.click}
+    >
+      {props.children}
+    </div>
+  );
+};
 
-const button = props => {
-    let myClass = [];
-    myClass.push(classes.Button)
-    myClass.push(fonts.Caption)
-    if(props.myClass.includes("goldBorder"))
-        myClass.push(classes.GoldBorder);
-    if(props.myClass.includes("goldBackground"))
-        myClass.push(classes.GoldBackground);
-    if(props.myClass.includes("transparentBackground"))
-        myClass.push(classes.TransparentBackground)
-    if(props.myClass.includes("whiteText"))
-        myClass.push(fonts.White)
-    if(props.myClass.includes("goldText"))
-        myClass.push(fonts.Gold)
-    if(props.myClass.includes("smallBtn"))
-        myClass.push(classes.SmallButton)
-    if(props.myClass.includes("mediumBtn"))
-        myClass.push(classes.MediumButton)
-    return(
-        <div className={myClass.join(' ')} style={props.style} onClick={props.click}>
-            {props.children}
-        </div>
-    )
-    }
-
-export default button
+export default button;
