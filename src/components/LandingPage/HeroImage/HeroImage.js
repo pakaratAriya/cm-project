41b70b8013e280fed871b980a0f488a9052cbfef
Kim Cyrus Esquibel <kimcyruse@Kims-Air.hitronhub.home>
2020-05-19 05:25:53 -0700

    Modified HeroImage Component

--- src/components/LandingPage/HeroImage/HeroImage.js
+++ src/components/LandingPage/HeroImage/HeroImage.js
@@ -1,42 +1,15 @@
-import React from 'react'
-import classes from './HeroImage.module.css'
-import fonts from '../../../globalCss/Fonts.module.css'
-import Button from '../../Button/Button'
+import React from "react";
+import classes from "./HeroImage.module.css";
+import fonts from "../../../globalCss/Fonts.module.css";
+import Button from "../../Button/Button";
+import SearchHome from "./SearchHome/SearchHome";
+import Header from "../../Header/Header";
 
-const heroImage = props => (
-    <div className={classes.HeroImage}>
-        <div className={classes.GridWrapper}>
-            <div className={[classes.LogoItem, fonts.H1, fonts.White].join(' ')}>
-                CM
-            </div>
-            <div className={[classes.Navigation, fonts.Subtitle, fonts.White].join(' ')}>
-                <span className={classes.navLink}>ACTIVE</span>
-                <span className={classes.navLink}>MLS SEARCH</span>
-                <span className={classes.navLink}>SOLD</span>
-                <span className={classes.navLink}>CONTACT</span>
-            </div>
-            <Button 
-                myClass={["mediumBtn", "goldBorder", "transparentBackground", "whiteText"]} 
-                style={{
-                    gridColumn: "20/24",
-                    alignSelf: "end"
-                }}>LET'S START</Button>
-        </div>
-        <div className={classes.SearchWrapper}>
-            <p className={[classes.SearchText, fonts.H2, fonts.White].join(' ')}>Let us find your next Home</p>
-            <input 
-                type='text' 
-                className={[classes.SearchInput, fonts.Caption, fonts.Gold].join(' ')} 
-                placeholder="Price, Neighborhood, City, Country"/>
-            <Button
-                myClass={["smallBtn", "goldBorder", "goldBackground", "whiteText"]}
-                style={{
-                    gridColumn: "20/24",
-                    display: "inline-block",
-                    marginLeft: "20px"
-                }}>Search</Button>
-        </div>
-    </div>
-)
+const heroImage = (props) => (
+  <div className={classes.HeroImage}>
+    <Header none="none" />
+    <SearchHome />
+  </div>
+);
 
 export default heroImage;
--- src/components/LandingPage/HeroImage/SearchHome/SearchHome.js
+++ src/components/LandingPage/HeroImage/SearchHome/SearchHome.js
@@ -1,11 +1,29 @@
-import React from 'react'
-import classes from './SearchHome.module.css'
+import React from "react";
+import classes from "./SearchHome.module.css";
+import fonts from "../../../../globalCss/Fonts.module.css";
+import Button from "../../../Button/Button";
 
-const searchHome = props => (
-    <div className={classes.SearchHome}>
-        Let us find your next Home
-    </div>
-)
+const searchHome = (props) => (
+  <div className={classes.SearchWrapper}>
+    <p className={[classes.SearchText, fonts.H1, fonts.White].join(" ")}>
+      LET US FIND YOUR <br /> NEXT HOME
+    </p>
+    <input
+      type="text"
+      className={[classes.SearchInput, fonts.Caption, fonts.Gold].join(" ")}
+      placeholder="Price, Neighborhood, City, Country"
+    />
+    <Button
+      myClass={["smallBtn", "goldBorder", "goldBackground", "whiteText"]}
+      style={{
+        gridColumn: "20/24",
+        display: "inline-block",
+        marginLeft: "20px",
+      }}
+    >
+      SEARCH
+    </Button>
+  </div>
+);
 
-export default searchHome
-
+export default searchHome;
