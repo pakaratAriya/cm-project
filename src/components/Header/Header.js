99b63eba85907d3d304d9bc521321d9630f7cb61
Kim Cyrus Esquibel <kimcyruse@Kims-Air.hitronhub.home>
2020-05-19 05:20:11 -0700

    Modified Header passing props none='none'

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,23 +1,31 @@
-import React from 'react'
-import classes from './Header.module.css'
-import fonts from '../../globalCss/Fonts.module.css'
-import Navigation from './Navigation/Navigation'
-import { withRouter} from 'react-router-dom'
+import React from "react";
+import classes from "./Header.module.css";
+import fonts from "../../globalCss/Fonts.module.css";
+import Navigation from "./Navigation/Navigation";
+import { withRouter } from "react-router-dom";
 
-const header = props => {
-    const onClickLogoHandler = ()=>{
-        props.history.push("/")
-        document.documentElement.scrollTop = 0
-    }
+const header = (props) => {
+  const onClickLogoHandler = () => {
+    props.history.push("/");
+    document.documentElement.scrollTop = 0;
+  };
 
-    return (
-        <div className={classes.Header}>
-            <div className={[classes.Title, fonts.Logo].join(' ')} onClick={onClickLogoHandler}>
-                CM
-            </div>
-            <Navigation/>
-        </div>
-    )
-}
+  return (
+    <div
+      className={classes.Header}
+      style={{
+        backgroundImage: props.none,
+      }}
+    >
+      <div
+        className={[classes.Title, fonts.Logo].join(" ")}
+        onClick={onClickLogoHandler}
+      >
+        CM
+      </div>
+      <Navigation />
+    </div>
+  );
+};
 
-export default withRouter(header)
+export default withRouter(header);
