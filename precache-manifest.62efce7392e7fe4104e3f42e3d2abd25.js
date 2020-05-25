8e399bbbd5bdb180b6525f2175e9c82268a9577a
pakaratAriya <pakarat_ariya@hotmail.com>
2020-05-25 14:02:31 -0700

    Updates

---
+++ precache-manifest.62efce7392e7fe4104e3f42e3d2abd25.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32565b9bfe01795506b81097e7204ab9",
+    "url": "/cm-project/index.html"
+  },
+  {
+    "revision": "382bd081fda05b3be68e",
+    "url": "/cm-project/static/css/main.986475f5.chunk.css"
+  },
+  {
+    "revision": "fcf80e99e538085d1bcb",
+    "url": "/cm-project/static/js/2.c3366c24.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/cm-project/static/js/2.c3366c24.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "382bd081fda05b3be68e",
+    "url": "/cm-project/static/js/main.d97d036c.chunk.js"
+  },
+  {
+    "revision": "9bba72ffc58b8675130d",
+    "url": "/cm-project/static/js/runtime-main.2ac6851b.js"
+  }
+]);
