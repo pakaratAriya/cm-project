c34585bc5cd1bf497b7b96f8bca35f2f558201aa
pakaratAriya <pakarat_ariya@hotmail.com>
2020-06-03 17:26:55 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/cm-project/precache-manifest.86b0c10458e859a987f37024b9a8363e.js"
+  "/cm-project/precache-manifest.31da8a44f4ff57f438cb6abece4a72a5.js"
 );
 
 self.addEventListener('message', (event) => {
